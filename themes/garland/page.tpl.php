4125403707667f22e323bfa0aa355921dfdb9b13
Navin <navin@example.com>
2016-08-02 16:18:17 +0530

    first commit

--- themes/garland/page.tpl.php
+++ themes/garland/page.tpl.php
@@ -337,10 +337,10 @@ if (in_array('reps', $user->roles) && strpos($uri,"/node/add/messagetoclient")) 
         var a = $(this).parent('td').parent('tr');//prop("checked",true);
           a.find('td').each (function() {
             if ( $(this).children( "input" ).length ) {
-              if($(this).children( "input" ).is(":checked")){
+              if($(this).children( "input" ).is(":checked")){ console.log("checked");
                     $(this).children( "input" ).prop("checked",true);
               }else{
-                  $(this).children( "input" ).prop("checked",false);
+                  $(this).children( "input" ).prop("checked",false); console.log("not checked");
               }
               console.log($(this).html());
             }  
