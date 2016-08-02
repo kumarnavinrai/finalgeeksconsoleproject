caae5ecc1a84d6f2fdbaad8661f8e6e93556e1da
Navin <navin@example.com>
2016-08-02 12:11:23 +0530

    first commit

--- themes/garland/page.tpl.php
+++ themes/garland/page.tpl.php
@@ -189,7 +189,10 @@ if (in_array('reps', $user->roles) && strpos($uri,"/node/add/messagetoclient")) 
             <?php 
               //change array here 
               $arrayforprocessing = array();
-              foreach($result as $k => $item) { echo strpos($item->user_name, '..'); die;
+              foreach($result as $k => $item) { 
+                echo strpos($item->user_name, '..'); 
+                echo substr($item->user_name, strpos($item->user_name, '..'), 3);
+                die;
                 $arrayforprocessing[$k]=array(
                                         'user_name'=>$item->user_name,
                                         'port'=>$item->port,
