f79c57288bf286ab33d0b8efa16fd78cd9070f1a
Navin <navin@example.com>
2016-08-03 15:58:55 +0530

    first commit

--- themes/garland/page--node--5.tpl.php
+++ themes/garland/page--node--5.tpl.php
@@ -294,7 +294,7 @@ if (in_array('reps', $user->roles) && strpos($uri,"/node/5")) { //print_r($_POST
                   ['Dates', 'Version 1', 'Version 2', 'Version 3', 'Version 4', 'Version 5', 'Version 6', 'Version 7', 'Version 8'],
                   <?php if(isset($arr) && $arr){ ?>
                   <?php foreach($arr as $key => $val){  ?>  
-                    ['<?php echo $key; ?>', <?php $p = current($val); print_r($p); echo isset($p[1])?$p[1]:0; ?>, <?php $p = next($val); echo isset($p[2])?$p[2]:0; ?>, <?php $p = next($val); echo isset($p[3])?$p[3]:0; ?>, <?php $p = next($val); echo isset($p[4])?$p[4]:0; ?>, <?php $p = next($val); echo isset($p[5])?$p[5]:0; ?>, <?php $p = next($val); echo isset($p[6])?$p[6]:0; ?>, <?php $p = next($val); echo isset($p[7])?$p[7]:0; ?>, <?php $p = next($val); echo isset($p[8])?$p[8]:0; ?>],
+                    ['<?php echo $key; ?>', <?php $p = current($val);  echo isset($p[1])?$p[1]:0; ?>, <?php $p = next($val); print_r($p); echo isset($p[2])?$p[2]:0; ?>, <?php $p = next($val); echo isset($p[3])?$p[3]:0; ?>, <?php $p = next($val); echo isset($p[4])?$p[4]:0; ?>, <?php $p = next($val); echo isset($p[5])?$p[5]:0; ?>, <?php $p = next($val); echo isset($p[6])?$p[6]:0; ?>, <?php $p = next($val); echo isset($p[7])?$p[7]:0; ?>, <?php $p = next($val); echo isset($p[8])?$p[8]:0; ?>],
                   <?php } ?>  
                   <?php } ?>
                   
