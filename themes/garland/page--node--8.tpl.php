b0062224fb339386a0f053ba124eaca1c65462ff
Navin <navin@example.com>
2016-08-03 12:38:06 +0530

    first commit

--- themes/garland/page--node--8.tpl.php
+++ themes/garland/page--node--8.tpl.php
@@ -1,10 +1,20 @@
-<?php  die("i am node 6");
+<?php  
 global $user;
 $url = $_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"];
 $uri = $_SERVER["REQUEST_URI"];
 $host = $_SERVER["HTTP_HOST"];
 $host = explode(".",$_SERVER["HTTP_HOST"]);
 $host = current($host);
+$previous_week = strtotime("-1 week +1 day");
+
+$start_week = strtotime("last sunday midnight",$previous_week);
+$end_week = strtotime("next saturday",$start_week);
+
+$start_week = date("Y-m-d",$start_week);
+$end_week = date("Y-m-d",$end_week);
+
+echo $start_week.' '.$end_week ;
+die;
 
 /*
   $arr["2016-04-04"][] = array("Version 1"=>9);
@@ -24,7 +34,7 @@ $adminselect = array("admin"=>"WHERE 1","adminone"=>"WHERE user_name LIKE '%..1%
 $adminselectforjs = array("admin"=>"","adminone"=>"PC..1","admintwo"=>"PC..2","adminthree"=>"PC..3","adminfour"=>"PC..4");
 
 
-if (in_array('reps', $user->roles) && strpos($uri,"/node/5")) { //print_r($_POST); die;
+if (in_array('reps', $user->roles) && strpos($uri,"/node/8")) { //print_r($_POST); die;
   $datequery = isset($_POST["from"]) && isset($_POST["to"])? " AND install_date BETWEEN '".$_POST["from"]."' AND '".$_POST["to"]."'":"";
   //$qry = "SELECT * FROM appdata ".$adminselect[$host].$datequery;//." BETWEEN #07/04/1996# AND #07/09/1996#;";
   $qry = "SELECT count(id) as noofinstalls, DATE_FORMAT(install_date,'%Y-%m-%d') as indate, version as version  FROM appdata ".$adminselect[$host].$datequery." GROUP BY version, indate ORDER BY indate, version";
