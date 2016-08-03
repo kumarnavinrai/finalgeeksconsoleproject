3870334b8c8fd16f956c8f571e1bf8acf6d75c42
Navin <navin@example.com>
2016-08-03 17:25:06 +0530

    first commit

--- themes/garland/page--node--9.tpl.php
+++ themes/garland/page--node--9.tpl.php
@@ -62,7 +62,7 @@ if (in_array('reps', $user->roles) && strpos($uri,"/node/9")) { //print_r($_POST
 
 $_POST["to"] = $end_week;
 ?>
-  <?php print render($page['header']); ?>
+ <?php print render($page['header']); ?>
 
   <div id="wrapper">
     <div id="container" class="clearfix">
@@ -146,12 +146,16 @@ $_POST["to"] = $end_week;
         </script>  
         <form id="formrange" class=".clsformrange" method="POST" style="display:none;">
           <label for="from">From</label>
-          <input type="text" id="from" name="from">
+          <input type="text" id="from" name="from" value="<?php echo $_POST["from"]; ?>" >
           <label for="to">to</label>
-          <input type="text" id="to" name="to"> 
+          <input type="text" id="to" name="to" value="<?php echo $_POST["to"]; ?>" > 
           <button>Go</button>     
         </form>  
         <h1>Last Week's Installation Stats</h1>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
       </p>
       <p>
 
@@ -291,25 +295,13 @@ $_POST["to"] = $end_week;
               </tr>
               <?php
 
-                $arr[$item->indate][] = array($item->version=>$item->noofinstalls);
+                $arr[$item->indate][$item->version] = $item->noofinstalls;
                 
                 
               ?>
                         
             <?php   } ?>
-            <!--
-                  $arr["2016-04-04"][] = array("Version 1"=>9);
-                $arr["2016-04-04"][] = array("Version 2"=>5);
-                print_r(current($arr["2016-04-04"])); 
-                print_r(next($arr["2016-04-04"])); 
-                print_r(next($arr["2016-04-04"])); 
-                 print_r(next($arr["2016-04-04"])); 
-                  print_r(next($arr["2016-04-04"])); 
-                   print_r(next($arr["2016-04-04"])); 
-                die;
-
-
-             -->
+     
             </table>  
             </div>
             <script type="text/javascript">
@@ -318,10 +310,10 @@ $_POST["to"] = $end_week;
 
               function drawChart() {
                 var data = google.visualization.arrayToDataTable([
-                  ['Dates', 'Version 1', 'Version 2', 'Version 3', 'Version 4', 'Version 5', 'Version 6', 'Version 7', 'Version 8'],
+                  ['Dates', 'Version 0','Version 1', 'Version 2', 'Version 3', 'Version 4', 'Version 5', 'Version 6', 'Version 7', 'Version 8'],
                   <?php if(isset($arr) && $arr){ ?>
                   <?php foreach($arr as $key => $val){ ?>  
-                    ['<?php echo $key; ?>', <?php $p = current($val); echo isset($p[1])?$p[1]:0; ?>, <?php $p = next($val); echo isset($p[2])?$p[2]:0; ?>, <?php $p = next($val); echo isset($p[3])?$p[3]:0; ?>, <?php $p = next($val); echo isset($p[4])?$p[4]:0; ?>, <?php $p = next($val); echo isset($p[5])?$p[5]:0; ?>, <?php $p = next($val); echo isset($p[6])?$p[6]:0; ?>, <?php $p = next($val); echo isset($p[7])?$p[7]:0; ?>, <?php $p = next($val); echo isset($p[8])?$p[8]:0; ?>],
+                    ['<?php echo $key; ?>', <?php $p = $val; echo isset($p[0])?$p[0]:0; ?>, <?php echo isset($p[1])?$p[1]:0; ?>, <?php echo isset($p[2])?$p[2]:0; ?>, <?php echo isset($p[3])?$p[3]:0; ?>, <?php echo isset($p[4])?$p[4]:0; ?>, <?php echo isset($p[5])?$p[5]:0; ?>, <?php echo isset($p[6])?$p[6]:0; ?>, <?php echo isset($p[7])?$p[7]:0; ?>, <?php echo isset($p[8])?$p[8]:0; ?>],
                   <?php } ?>  
                   <?php } ?>
                   
@@ -392,7 +384,7 @@ $_POST["to"] = $end_week;
               </tr>
               <?php
                
-                $arr[$item->unindate][] = array($item->version=>$item->noofinstalls);
+                $arr[$item->indate][$item->version] = $item->noofinstalls;
                 
               ?> 
             <?php   } ?>
@@ -405,10 +397,10 @@ $_POST["to"] = $end_week;
 
               function drawChart() {
                 var data = google.visualization.arrayToDataTable([
-                  ['Dates', 'Version 1', 'Version 2', 'Version 3', 'Version 4', 'Version 5', 'Version 6', 'Version 7', 'Version 8'],
+                  ['Dates', 'Version 0','Version 1', 'Version 2', 'Version 3', 'Version 4', 'Version 5', 'Version 6', 'Version 7', 'Version 8'],
                   <?php if(isset($arr) && $arr){ ?>
                   <?php foreach($arr as $key => $val){ ?>  
-                    ['<?php echo $key; ?>', <?php $p = current($val); echo isset($p[1])?$p[1]:0; ?>, <?php $p = next($val); echo isset($p[2])?$p[2]:0; ?>, <?php $p = next($val); echo isset($p[3])?$p[3]:0; ?>, <?php $p = next($val); echo isset($p[4])?$p[4]:0; ?>, <?php $p = next($val); echo isset($p[5])?$p[5]:0; ?>, <?php $p = next($val); echo isset($p[6])?$p[6]:0; ?>, <?php $p = next($val); echo isset($p[7])?$p[7]:0; ?>, <?php $p = next($val); echo isset($p[8])?$p[8]:0; ?>],
+                    ['<?php echo $key; ?>', <?php $p = $val; echo isset($p[0])?$p[0]:0; ?>, <?php echo isset($p[1])?$p[1]:0; ?>, <?php echo isset($p[2])?$p[2]:0; ?>, <?php echo isset($p[3])?$p[3]:0; ?>, <?php echo isset($p[4])?$p[4]:0; ?>, <?php echo isset($p[5])?$p[5]:0; ?>, <?php echo isset($p[6])?$p[6]:0; ?>, <?php echo isset($p[7])?$p[7]:0; ?>, <?php echo isset($p[8])?$p[8]:0; ?>],
                   <?php } ?>  
                   <?php } ?>
                   
