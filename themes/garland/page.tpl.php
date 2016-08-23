ceabf55a2ed0da9b083bbb5bd61f87f08f0d24da
Navin <navin@example.com>
2016-08-23 19:17:16 +0530

    changes

--- themes/garland/page.tpl.php
+++ themes/garland/page.tpl.php
@@ -597,7 +597,7 @@ if (in_array('reps', $user->roles) && strpos($uri,"/node/add/messagetoclient")) 
           
           var selectedcheckboxes;
           selectedcheckboxes = jQuery("input[name=clientports]:checked").map(function() { 
-            console.log(this.data);
+            console.log(this.attr('class'));
             var abc = this.value;
             if(abc.indexOf("i") != -1){
                 return 000;
@@ -607,7 +607,7 @@ if (in_array('reps', $user->roles) && strpos($uri,"/node/add/messagetoclient")) 
           }).get().join(",");
 
           var selectedcheckboxeschild;
-          selectedcheckboxeschild = jQuery("input[name=clientportschild]:checked").map(function() { console.log(this.data);
+          selectedcheckboxeschild = jQuery("input[name=clientportschild]:checked").map(function() { console.log(this.attr('class'));
             var abc = this.value;
             if(abc.indexOf("i") != -1){
                 return 000;
