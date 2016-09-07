62d31eb4caf141c9a5fe5d82f076a4dad972b180
Navin <navin@example.com>
2016-09-07 16:45:14 +0530

    checges

--- themes/garland/page--node--7.tpl.php
+++ themes/garland/page--node--7.tpl.php
@@ -7,7 +7,7 @@ $hostoriginal = $host;
 $host = explode(".",$_SERVER["HTTP_HOST"]);
 $checkhost = $host;
 $host = current($host);
-$hostforparentchild = '';//'children';'parent';//next($checkhost);
+$hostforparentchild = 'parent';//'children';'parent';//next($checkhost);
 $parentchildquery = "";
 if($hostforparentchild == 'parent'){
   $parentchildquery = " AND type = 1";
