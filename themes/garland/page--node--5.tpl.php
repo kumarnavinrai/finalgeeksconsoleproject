77c77c0985f6d18460a11fe4a3191ae2708bcb95
Navin <navin@example.com>
2016-09-07 12:45:33 +0530

    checges

--- themes/garland/page--node--5.tpl.php
+++ themes/garland/page--node--5.tpl.php
@@ -73,6 +73,60 @@ if (in_array('reps', $user->roles) && strpos($uri,"/node/5")) { //print_r($_POST
   //SELECT *  FROM `temponline` WHERE `user_name` LIKE '%PC..1%'
   $resulttwo = db_query($qrytwo);
   $_SESSION["perm"]="a";
+
+  $qrydetailsofinstall = "SELECT * FROM appdata ".$adminselect[$host].$datequery." AND uninstall_date != '' ";
+  $qrydetailsofinstallcount = "SELECT count(id) FROM appdata ".$adminselect[$host].$datequery." AND uninstall_date != '' ";
+
+  /* Get total number of records */
+         $rec_limit = 10;
+         $sql = $qrydetailsofinstallcount;
+         $retval = db_query( $sql );
+         
+         //if there is any count of record then do anything else
+         if( $retval ) 
+         {
+          echo "<pre>"; print_r($retval); die;
+           $row = mysql_fetch_array($retval, MYSQL_NUM );
+           $rec_count = $row[0];
+           
+           if( isset($_GET{'page'} ) ) {
+              $page = $_GET{'page'} + 1;
+              $offset = $rec_limit * $page ;
+           }else {
+              $page = 0;
+              $offset = 0;
+           }
+           
+           $left_rec = $rec_count - ($page * $rec_limit);
+           $sql = "SELECT emp_id, emp_name, emp_salary ". 
+              "FROM employee ".
+              "LIMIT $offset, $rec_limit";
+              
+           $retval = mysql_query( $sql, $conn );
+           
+           if(! $retval ) {
+              die('Could not get data: ' . mysql_error());
+           }
+           
+           while($row = mysql_fetch_array($retval, MYSQL_ASSOC)) {
+              echo "EMP ID :{$row['emp_id']}  <br> ".
+                 "EMP NAME : {$row['emp_name']} <br> ".
+                 EMP SALARY : {$row['emp_salary']} <br> ".
+                 "--------------------------------<br>";
+           }
+           
+           if( $page > 0 ) {
+              $last = $page - 2;
+              echo "<a href = \"$_PHP_SELF?page = $last\">Last 10 Records</a> |";
+              echo "<a href = \"$_PHP_SELF?page = $page\">Next 10 Records</a>";
+           }else if( $page == 0 ) {
+              echo "<a href = \"$_PHP_SELF?page = $page\">Next 10 Records</a>";
+           }else if( $left_rec < $rec_limit ) {
+              $last = $page - 2;
+              echo "<a href = \"$_PHP_SELF?page = $last\">Last 10 Records</a>";
+           }
+
+       }//if count check if ends
 }
 
 ?>
